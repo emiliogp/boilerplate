7833a2d1e195e306ae36d45ca80a4779bd3fd610
eric <eric.basley@redpelicans.com>
2017-03-22 15:20:56 +0100

    Add content

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Grid, Jumbotron, Row, Col } from 'react-bootstrap';
 import { BoardPanel } from '../Board';
+import { Header, HeaderLeft, HeaderRight } from '../Header';
+import { Icon, Title } from '../Widgets';
+
 import './app.css';
 
-const App = ({ board }) => (
-  <Grid className="content">
-    <Jumbotron>
+const App = ({ board, player }) => (
+  <Grid>
+    <Row>
+      <Header player={player}>
+        <HeaderLeft>
+          <Icon type='trophy' />
+          <Title name='TicTacToe' />
+        </HeaderLeft>
+        <HeaderRight>
+        </HeaderRight>
+      </Header>
+    </Row>
+    <Jumbotron className="content">
       <Row>
         <Col md={6} xs={12}>
           <BoardPanel board={board} />
@@ -17,6 +30,7 @@ const App = ({ board }) => (
 
 App.propTypes = {
   board: React.PropTypes.array.isRequired,
+  player: React.PropTypes.object.isRequired,
 };
 
 export default App;
