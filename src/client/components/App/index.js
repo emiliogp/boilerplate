17d621286aee7d145fd6da7a63a8e5f6d82d22e2
eric <eric.basley@redpelicans.com>
2017-03-22 15:27:51 +0100

    Enhance App

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Grid, Jumbotron, Row, Col } from 'react-bootstrap';
 import { BoardPanel } from '../Board';
-import { Header, HeaderLeft, HeaderRight } from '../Header';
-import { Icon, Title } from '../Widgets';
 
 import './app.css';
 
 const App = ({ board, player }) => (
   <Grid>
-    <Row>
-      <Header player={player}>
-        <HeaderLeft>
-          <Icon type='trophy' />
-          <Title name='TicTacToe' />
-        </HeaderLeft>
-        <HeaderRight>
-        </HeaderRight>
-      </Header>
-    </Row>
     <Jumbotron className="content">
       <Row>
         <Col md={6} xs={12}>
